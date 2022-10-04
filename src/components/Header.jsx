f55172a68f3af3dd33e820534fe24f7d044458fb
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-10-05 01:06:45 +0400

    create a spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
     }
   }
   return (
-    <div className="bg-white border-b mt-1 shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b mt-1 shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
