e466eda59a4a009176875d078bb6eafee2a69f5c
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-10-20 02:48:54 +0400

    create the places for rent and sale pages

---
+++ src/pages/Category.jsx
@@ -0,0 +1,110 @@
+import {
+    collection,
+    doc,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+    startAfter,
+    where,
+  } from "firebase/firestore";
+  import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+  import { toast } from "react-toastify";
+  import ListingItem from "../components/ListingItem";
+  import Spinner from "../components/Spinner";
+  import { db } from "../firebase";
+  
+  export default function Category() {
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [lastFetchListing, setlastFetchListing] = useState(null);
+    const params = useParams()
+    useEffect(() => {
+      async function fitchListings() {
+        try {
+          const listingRef = collection(db, "listings");
+          const q = query(
+            listingRef,
+            where("type", "==", params.categoryName),
+            orderBy("timestamp", "desc"),
+            limit(8)
+          );
+          const querySnap = await getDocs(q);
+          const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+          setlastFetchListing(lastVisible);
+          const listings = [];
+          querySnap.forEach((doc) => {
+            return listings.push({
+              id: doc.id,
+              data: doc.data(),
+            });
+          });
+          setListings(listings);
+          setLoading(false);
+        } catch (error) {
+          toast.error("Could not fitch listing");
+        }
+      }
+      fitchListings();
+    }, [params.categoryName]);
+  
+    async function onFetchMoreListings(){
+      try {
+        const listingRef = collection(db, "listings");
+        const q = query(
+          listingRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          startAfter(lastFetchListing),
+          limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setlastFetchListing(lastVisible);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings((prevState)=>[...prevState, ...listings]);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fitch listing");
+      }
+    }
+    
+  
+    return (
+      <div className="max-w-6xl mx-auto px-3">
+        <h1 className="text-3xl text-center mt-6 font-bold mb-6 ">{params.categoryName === "rent" ? "Places for rent" : "Places for sale"}</h1>
+        {loading ? (
+          <Spinner />
+        ) : listings && listings.length > 0 ? (
+          <>
+            <main className="m-2 mb-6 ">
+              <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {listings.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    listing={listing.data}
+                    id={listing.id}
+                  />
+                ))}
+              </ul>
+            </main>
+            {lastFetchListing && (
+              <div className="flex justify-center items-center">
+                <button onClick={onFetchMoreListings} className="bg-white px-3 py-1.5  text-gray-700 border border-gray-300 mb-6 mt-6 hover:border-slate-500 rounded transition duration-150 ease-in-out   ">Load More</button>
+              </div>
+            )}
+          </>
+        ) : (
+          <p>There is no current offers</p>
+        )}
+      </div>
+    );
+  }
+  
